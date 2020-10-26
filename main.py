a333a2572e55ba4b2ba011bc533919affe500181
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-26 18:27:55 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def bernoulli(p):
+  # Your code to generate the bernoulli random variable goes here
+
+def binomial(n,p) :
+  # Your code to generate the binomial random variable goes here.
+  
+
+print( binomial(5,0.5), binomial(5,0.5), binomial(5,0.5) )
