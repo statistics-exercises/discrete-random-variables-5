ebcf5dcfc8bebd2a1c95b64ee69d7d38d0a33647
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 10:25:57 +0100

    Added answer

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import numpy as np
 
 def bernoulli(p):
   # Your code to generate the bernoulli random variable goes here
+  if np.random.uniform(0,1)<p : return 1
+  return 0
 
 def binomial(n,p) :
   # Your code to generate the binomial random variable goes here.
-  
+  var = 0
+  for i in range(n) : var = var + bernoulli(p)
+  return var
 
 print( binomial(5,0.5), binomial(5,0.5), binomial(5,0.5) )
